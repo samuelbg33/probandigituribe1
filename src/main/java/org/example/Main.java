bd91ae04de82646f1c4d10e3fe740514d9ee50b0
EstebanHernandezA16 <EstebanHernandez23201@hotmail.com>
2023-11-28 11:30:06 -0500

    se inicia preyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+ public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Natillera la abuelita Toretto
+        //¿Que nececito para procedor la natillera de 1 cliente?
+        //ENTRADAS
+        int idenrificadorCliente=144442;
+        long  documentoIdentidad=1017927144123L;
+        byte  edadCliente=34;
+        short cuotaMensualCliente=25000;
+        float tasaInteresfijo=0.1f;
+        char calificacionCliente='A';
+
+
+    }
+}
